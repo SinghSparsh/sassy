e986ede32908bfcb8f321b4648c5e793815cb0e6
SinghSparsh <97666347+SinghSparsh@users.noreply.github.com>
2024-08-11 16:28:01 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,45 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import {
+  ClerkProvider,
+  SignIn,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
+});
+
+export const metadata: Metadata = {
+  title: "SAAS-APP",
+  description: "AI BASED IMAGE TRANSFORMATION AND GENERATION TOOL",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          <main className="auth">
+            <SignedOut>
+              <SignIn routing="hash" />
+            </SignedOut>
+
+            <SignedIn>{children}</SignedIn>
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
